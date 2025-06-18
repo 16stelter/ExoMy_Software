708aa86c2c953879618e5d3b278546f7761f2846
Sebastian Stelter <sebastian.stelter@cranfield.ac.uk>
2025-06-18 13:51:29 +0100

    Fix incorrect namespace assignment in launch

--- exomy/launch/exomy.launch.py
+++ exomy/launch/exomy.launch.py
@@ -8,7 +8,6 @@ from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 
-namespace_ = 'exomy'
 
 def launch_setup(context, *args, **kwargs):
     # Get the path to the exomy.yaml parameter file
@@ -21,7 +20,7 @@ def launch_setup(context, *args, **kwargs):
         package='exomy',
         executable='robot_node',
         name='robot_node',
-        namespace=namespace_,
+        namespace=namespace,
         parameters=[configured_params],
         output='screen'
     )
@@ -29,21 +28,21 @@ def launch_setup(context, *args, **kwargs):
         package='joy',
         executable='joy_node',
         name='joy_node',
-        namespace=namespace_,
+        namespace=namespace,
         output='screen'
     )
     joystick = Node(
         package='exomy',
         executable='joystick_parser_node',
         name='joystick_parser_node',
-        namespace=namespace_,
+        namespace=namespace,
         output='screen'
     )
     motors = Node(
         package='exomy',
         executable='motor_node',
         name='motor_node',
-        namespace=namespace_,
+        namespace=namespace,
         parameters=[configured_params],
         output='screen'
     )
@@ -52,7 +51,7 @@ def launch_setup(context, *args, **kwargs):
 
 def generate_launch_description():
     declare_namespace_cmd = DeclareLaunchArgument(
-        'namespace', default_value='exomy', description='Top-level namespace'
+        'namespace', default_value='', description='Top-level namespace'
     )
 
     return LaunchDescription([
@@ -60,21 +59,21 @@ def generate_launch_description():
         OpaqueFunction(function=launch_setup)
     ])
 
-
 def prepend_namespace_to_yaml(input_file, namespace):
     with open(input_file, 'r') as f:
         data = yaml.safe_load(f)
 
     namespaced_data = {}
     for node_name, node_config in data.items():
-        namespaced_key = f'/{namespace}/{node_name}'
+        if namespace:
+            namespaced_key = f'/{namespace}/{node_name}'
+        else:
+            namespaced_key = f'/{node_name}'
         namespaced_data[namespaced_key] = node_config
 
     tmp_file = NamedTemporaryFile(delete=False, mode='w', suffix='.yaml')
     yaml.dump(namespaced_data, tmp_file)
     tmp_file.close()
 
-   # with open(tmp_file.name, 'r') as f:
-   #     print(f.read())  # print temp file content
-
     return tmp_file.name
+
